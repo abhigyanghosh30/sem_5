efee58c5b54cda9f3a28ccdbc0c24402c98a67fa
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2019-09-13 04:08:14 +0530

    Tokenization Works!

--- NLP/Assignment 3-4/tokenizer.py
+++ NLP/Assignment 3-4/tokenizer.py
@@ -1,16 +1,35 @@
 import re
 corpus3_sentences = []
-with open('corpus3.txt','r') as f:
-    corpus3_sentences.append(f.read())
+f = open('corpus3.txt','r')
+corpus3_sentences=f.readlines()
+f.close()
+
+corpus4_sentences = []
+f = open('corpus4.txt','r')
+corpus4_sentences=f.readlines()
+f.close()
 
 def word_tokenizer(sentence):
     scanner = re.Scanner([
-        (r"[a-z]+", lambda scanner,token: ("WORD",token))
+        (r"[a-zA-Z0-9]+@([a-zA-Z0-9]+\.*){2,3}",lambda scanner,token: ("EMAIL",token)),
+        (r"http[s]*://[A-Z0-9a-z./]*",lambda scanner,token: ("LINK",token)),
+        (r"#[A-Za-z0-9]+",lambda scanner,token: ("HASHTAG",token)),
+        (r"@[A-Za-z0-9]+",lambda scanner,token: ("MENTION",token)),
+        (r"[A-Za-z]+", lambda scanner,token: ("WORD",token)),
+        (r"\s",lambda scanner,token: ("SPACE",token)),
+        (r"[0-9]+", lambda scanner,token: ("NUMBERS",token)),
+        (r"[,.?`’\-]+", lambda scanner,token: ("PUNC",token)),
+
     ])
 
     results,remainder = scanner.scan(sentence)
     print(results)
 
 if __name__ == "__main__":
-    for line in corpus3_sentences:
+    # for line in corpus3_sentences[:3]:
+    #     print(line)
+    #     word_tokenizer(line)
+    
+    for line in corpus4_sentences[:6]:
+        print(line)
         word_tokenizer(line)
