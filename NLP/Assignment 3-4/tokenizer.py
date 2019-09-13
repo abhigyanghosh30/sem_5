541b72e871694a508b5904db0b3ed8641645499e
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2019-09-13 17:14:22 +0530

    emojis tokenized

--- NLP/Assignment 3-4/tokenizer.py
+++ NLP/Assignment 3-4/tokenizer.py
@@ -1,6 +1,4 @@
 import re
-# import unicodedata
-# from unidecode import unidecode
 
 corpus3_sentences = []
 f = open('corpus3.txt','r')
@@ -34,29 +32,26 @@ f.close()
 
 def word_tokenizer(sentence):
     scanner = re.Scanner([
+        (b"[\x80-\xff]+", lambda scanner,token: ("EMOJI",token)),
         (r"[a-zA-Z0-9]+@([a-zA-Z0-9]+\.*){2,3}",lambda scanner,token: ("EMAIL",token)),
         (r"http[s]*://[A-Z0-9a-z./]*",lambda scanner,token: ("LINK",token)),
         (r"#[A-Za-z0-9]+",lambda scanner,token: ("HASHTAG",token)),
         (r"@[A-Za-z0-9]+",lambda scanner,token: ("MENTION",token)),
         (r"[A-Za-z_]+", lambda scanner,token: ("WORD",token)),
         (r"$[0-9]+(.[0-9]+)*",lambda scanner,token: ("CURRENCY",token)),
-        (r"\s",lambda scanner,token: ("SPACE",token)),
+        (r"\s",lambda scanner,token: None),
         (r"[0-9]+", lambda scanner,token: ("NUMBERS",token)),
         (r"[,.?!`'’\-@&;\(\):\/#]+", lambda scanner,token: ("PUNC",token)),
-        (u"(\ud83d[\ude00-\ude4f])=", lambda scanner,token: ("EMOJI",token)),
-        (u"(\ud83c[\udf00-\uffff])+", lambda scanner,token: ("EMOJI",token)),
-        (u"(\ud83d[\u0000-\uddff])+", lambda scanner,token: ("EMOJI",token)),
-        (u"(\ud83d[\ude80-\udeff])+", lambda scanner,token: ("EMOJI",token)),
-        (u"(\ud83c[\udde0-\uddff])+", lambda scanner,token: ("EMOJI",token)),
     ])
     results,remainder = scanner.scan(sentence)
     print(results)
-    for word in remainder.split(r"\\"):
-        print(word)
+    # remainder.encode('utf-8')
+    # for word in remainder.split(r"\\"):
+    # print(scanner.scan(remainder))
     # deEmojify(remainder)
     # results,remainder = scanner.scan(sentence)
     # print(results)
-    print()
+    print(remainder)
 
 if __name__ == "__main__":
     # for line in corpus3_sentences[:3]:
@@ -65,4 +60,4 @@ if __name__ == "__main__":
     
     for line in corpus4_sentences:
         print(line)
-        word_tokenizer(line)
+        word_tokenizer(line.encode('utf-8'))
