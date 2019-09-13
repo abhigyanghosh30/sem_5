d77082218c7f8df328808ffd14a54347cd8338b9
abhigyanghosh30 <abhigyanghosh30@gmail.com>
2019-09-14 01:00:21 +0530

    final tokenized

--- NLP/Assignment 3-4/tokenizer.py
+++ NLP/Assignment 3-4/tokenizer.py
@@ -11,53 +11,36 @@ corpus4_sentences=f.readlines()
 f.close()
 
 
-# def deEmojify(inputString):
-#     returnString = ""
-
-#     for character in inputString:
-#         try:
-#             character.encode("ascii")
-#             returnString += character
-#         except UnicodeEncodeError:
-#             replaced = unidecode(str(character))
-#             if replaced != '':
-#                 returnString += replaced
-#             else:
-#                 try:
-#                      returnString += "[" + unicodedata.name(character) + "]"
-#                 except ValueError:
-#                      returnString += "[x]"
-
-#     return returnString
-
 def word_tokenizer(sentence):
     scanner = re.Scanner([
-        (b"[\x80-\xff]{2,4}", lambda scanner,token: ("EMOJI",token)),
+        (b"[\x80-\xff]+", lambda scanner,token: ("EMOJI",token)),
         (r"[a-zA-Z0-9]+@([a-zA-Z0-9]+\.*){2,3}",lambda scanner,token: ("EMAIL",token)),
         (r"http[s]*://[A-Z0-9a-z./]*",lambda scanner,token: ("LINK",token)),
-        (r"#[A-Za-z0-9]+",lambda scanner,token: ("HASHTAG",token)),
-        (r"@[A-Za-z0-9]+",lambda scanner,token: ("MENTION",token)),
-        (r"[A-Za-z_]+", lambda scanner,token: ("WORD",token)),
+        (r"#\S+",lambda scanner,token: ("HASHTAG",token)),
+        (r"@\S+",lambda scanner,token: ("MENTION",token)),
+        (r'[A-Z][a-z]*\.', lambda scanner, token: ("NNP",token)),
+        (r"[0-9]+% | [0-9]+\ %", lambda scanner,token: ("PERCENT",token)),
+        (r"[0-9.]+", lambda scanner,token: ("DECIMAL",token)),
+        (r"\w+", lambda scanner,token: ("WORD",token)),
         (r"$[0-9]+(.[0-9]+)*",lambda scanner,token: ("CURRENCY",token)),
-        (r"\s",lambda scanner,token: None),
+        (r"\s+",lambda scanner,token: None),
+        (r"\n",lambda scanner,token: None),
+        (r"\\n",lambda scanner,token: None),
         (r"[0-9]+", lambda scanner,token: ("NUMBERS",token)),
-        (r"[,.?!`'’\-@&;\(\):\/#]+", lambda scanner,token: ("PUNC",token)),
+        (r"[\",.?!`'’\-@&;\(\):\/#$\*\|=]+", lambda scanner,token: ("PUNC",token)),
     ])
     results,remainder = scanner.scan(sentence)
-    print(results)
-    # remainder.encode('utf-8')
-    # for word in remainder.split(r"\\"):
-    # print(scanner.scan(remainder))
-    # deEmojify(remainder)
-    # results,remainder = scanner.scan(sentence)
-    # print(results)
-    print(remainder)
+
+    if(len(remainder)>0):
+        print(sentence)
+        print(results)
+        print(remainder)
 
 if __name__ == "__main__":
-    # for line in corpus3_sentences[:3]:
-    #     print(line)
-    #     word_tokenizer(line)
-    
-    for line in corpus4_sentences:
-        print(line)
+    for line in corpus3_sentences:
+        # print(line)
         word_tokenizer(line.encode('utf-8'))
+    
+    # for line in corpus4_sentences:
+    #     word_tokenizer(line.encode('utf-8'))
+    # word_tokenizer(b'Rev. St. Francis of Assisi')
